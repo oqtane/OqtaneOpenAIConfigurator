d51a3661d46e03a43f730bcf00bae56bf7d48357
Michael Washington <webmaster@adefwebserver.com>
2023-06-11 10:06:00 -0700

    Add project files.

---
+++ Models/TreeNode.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OqtaneOpenAIConfigurator
+{
+    public class TreeNode
+    {
+        [Key]
+        public int Id { get; set; }
+        public string NodeName { get; set; }
+        public List<TreeNode> Children { get; set; }
+    }
+}
